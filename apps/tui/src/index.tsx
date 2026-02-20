14ecdbb56aab0a9aa1fbdb72d880c65ae7889164
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2026-02-21 00:31:41 +0530

    add an input box

--- apps/tui/src/index.tsx
+++ apps/tui/src/index.tsx
@@ -1,12 +1,38 @@
-import { createCliRenderer, TextAttributes } from "@opentui/core";
+import { createCliRenderer } from "@opentui/core";
 import { createRoot } from "@opentui/react";
+import { tokyonight } from "./utils/themes";
+import { useCallback, useState } from "react"
 
 function App() {
+  const [msg, setMsg] = useState("")
+  const [messages, setMessages] = useState<{ text: string }[]>([])
+
+  const handleSubmit = useCallback(() => {
+    setMessages([...messages, { text: msg }])
+    setMsg("")
+  }, [msg])
+
+
   return (
     <box alignItems="center" justifyContent="center" flexGrow={1}>
-      <box justifyContent="center" alignItems="flex-end">
-        <ascii-font font="tiny" text="OpenTUI" />
-        <text attributes={TextAttributes.DIM}>What will you build?</text>
+      {
+        messages.length <= 0 &&
+        <box justifyContent="center" alignItems="flex-end" flexGrow={1}>
+          <ascii-font font="tiny" text="quasar" color={tokyonight.green} />
+        </box>
+      }
+      <box style={{ width: "100%", flexGrow: 1, padding: 1 }} >
+        {messages.map((msg) => (<text width={"100%"} padding={0.5}>{msg.text}</text>))}
+      </box>
+      <box style={{ border: true, borderStyle: "rounded", borderColor: tokyonight.blue, width: "100%", minHeight: 3, position: "absolute", bottom: 2 }}>
+        <input
+          paddingX={1}
+          placeholder="Ask me anything"
+          focused
+          value={msg}
+          onInput={setMsg}
+          onSubmit={handleSubmit}
+        />
       </box>
     </box>
   );
---
+++ apps/tui/src/utils/themes.ts
@@ -0,0 +1,12 @@
+import { RGBA } from "@opentui/core"
+
+export const tokyonight = {
+  black: RGBA.fromHex('#32344a'),
+  red: RGBA.fromHex('#f7768e'),
+  green: RGBA.fromHex('#9ece6a'),
+  yellow: RGBA.fromHex('#e0af68'),
+  blue: RGBA.fromHex('#7aa2f7'),
+  magenta: RGBA.fromHex('#ad8ee6'),
+  cyan: RGBA.fromHex('#449dab'),
+  white: RGBA.fromHex('#787c99'),
+}
